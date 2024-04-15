2e7f4653c71db394bf14c00a84f24ce2fd130290
xec-cm <fcatala@irsicaixa.es>
2024-04-15 12:08:48 +0200

    Update issue_11.R

--- R/issue_11.R
+++ R/issue_11.R
@@ -1,147 +1,173 @@
 #' Read HUMAnN3 output file and create SummarizedExperiment objects
 #'
-#' This function reads an output file from HUMAnN3 and creates SummarizedExperiment objects
-#' containing normalized abundance values for groups (gene families or pathway categories)
-#' and stratifications (breakdown of groups by species).
+#' This function reads an output file from HUMAnN3 and creates
+#' SummarizedExperiment objects containing normalized abundance values for
+#' groups (gene families or pathway categories) and stratifications (breakdown
+#' of groups by species).
 #'
 #' @param file_path Path to the HUMAnN3 output TSV file.
 #' @param metadata A CSV file containing sample information.
 #'
-#' @return A list containing two SummarizedExperiment objects: one for groups and one for stratifications.
+#' @return A list containing two SummarizedExperiment objects: one for groups
+#'   and one for stratifications.
 #'
 #' @export
 #'
 #' @examples
+#' # Def data paths
+#' metadata <- system.file("extdata", "ex_meta.csv", package = "microfunk")
 #' file_path <-
 #'   system.file("extdata", "All_genefam_cpm_kegg.tsv", package = "microfunk")
-#' metadata <-
-#'   system.file("extdata", "ex_meta.csv", package = "microfunk")
+#'
+#' # Read HUMAnN3
 #' draft_read_humann(file_path, metadata)
 draft_read_humann <- function(file_path, metadata){
 
-  # check input
-  if (missing(file_path)){cli::cli_abort(c("x"="File path is missing"))}
-  if (missing(metadata)){cli::cli_abort(c("x"="Metadata is missing"))}
-
-  # check file path exists
-  if (!file.exists(file_path)) {
-    cli::cli_abort(c("x"="File does not exist"))
-  }
-
-  # check metadata input
-  if (tools::file_ext(metadata) != "csv"){
-    cli::cli_abort(c("x"="Unsupported metadata file extension.",
-                     "i"="Please provide a CSV file."))
-  }
+  # check input paths
+  .humann_path_checks(file_path, metadata)
 
   # read file
-  tbl <-
-    data.table::fread(file_path, sep="\t") %>%
-    tibble::as_tibble()
+  tbl <- data.table::fread(file_path, sep = "\t") %>% tibble::as_tibble()
 
-  # check header
-  header <- colnames(tbl)[1]
-  if (!(stringr::str_detect(header, "^# Gene Family$") ||
-        stringr::str_detect(header, "^# Pathway$"))) {
-    cli::cli_abort(c("x"="The header of the file should be '# Gene Family' or '# Pathway'."))
-  }
-
-  # check number of columns
-  if (ncol(tbl) < 2) { cli::cli_abort(c("x"="ncol(input) < 2.",
-                        "i"="The file should contain at least two columns.")) }
-
-  # check number of rows
-  if (nrow(tbl) <= 1) {
-    cli::cli_abort(c("x"="The file does not contain any data",
-                    "i"="Ensure the file is not empty."))
-  }
-
-  # check column classes
-  class_1 <- class(tbl[[1]]) == "character"
-  class_2 <- purrr::map_lgl(tbl[-1], ~class(.x) == "numeric") %>%
-    unname()
-
-  if (!class_1 || !all(class_2)) {
-    cli::cli_abort(c("x" = "Column classes are not correct.",
-               "i" = "First column should be 'character' and the rest should be 'numeric'."))
-  }
+  # check humann tbl
+  .humann_tbl_checks(tbl)
 
   # change column names
-  pattern <- "_Abundance.*$"
-  names <- colnames(tbl) %>%
-    stringr::str_remove(pattern)
-  colnames(tbl) <- names
-
-  # new tibble without stratification
-  tbl_group <- tbl %>%
-    dplyr::filter(!stringr::str_detect(.data[[header]],"[|]")) %>%
-    dplyr::select(gtools::mixedsort(names(.))) %>%
-    tibble::as_tibble()
-
-  # new tibble with stratified values
-  tbl_strat <- tbl %>%
-    dplyr::filter(stringr::str_detect(.data[[header]], "[|]")) %>%
-    dplyr::select(gtools::mixedsort(names(.))) %>%
-    tibble::as_tibble()
+  tbl <- dplyr::rename_with(tbl, ~ stringr::str_remove(., "_Abundance.*$"))
 
   # check data normalization
-  sum_counts <- tbl_group %>%
-    dplyr::select(-1) %>%
-    colSums() %>%
-    unname()
+  norm_method <- .extract_norm_method(tbl)
 
-  # classify columns in rpk/cpm
-  classification <- ifelse(dplyr::between(sum_counts, 99900, 1001000), "cpm", "rpk")
-
-  # check for different units across columns
-  mixed <- length(unique(classification)) == 1
-  if (!mixed){cli::cli_abort(c("x"="Columns have mixed units.",
-              "i"="Please ensure all samples have either 'rpk' or 'cpm' units."))}
-
-  # normalize rpk to cpm if needed
-  if(unique(classification) == "rpk"){
-
-    # scale groups
-    norm_group <- tbl_group %>%
-      dplyr::select(-1) %>%
-      purrr::map2_dfc(., sum_counts, ~ .x / .y * 1e6) %>%
-      dplyr::bind_cols(tbl_group[1],.) %>%
-      tibble::as_tibble()
-
-    # scale stratification
-    norm_strat <- tbl_strat %>%
-      dplyr::select(-1) %>%
-      purrr::map2_dfc(., sum_counts, ~ .x / .y * 1e6) %>%
-      dplyr::bind_cols(tbl_strat[1],.) %>%
-      tibble::as_tibble()
+  if (norm_method == "rpk") {
+    tbl <- .rpk2cpm(tbl)
   }
-  norm_group <- tbl_group
-  norm_strat <- tbl_strat
 
-  # read metadata
-  m_data <- data.table::fread(metadata) %>%
+  # read and check metadata
+  m_data <-
+    data.table::fread(metadata) %>%
     tibble::as_tibble() %>%
-    dplyr::arrange(readr::parse_number(.[[1]]))
+    .humann_metadata_check(tbl) %>%
+    data.frame(row.names = 1)
 
-  # check metadata content
-  if (ncol(m_data) < 2) { cli::cli_abort(c("x"="ncol(metadata) < 2.",
-                       "i"="Metadata should contain at least one annotation.")) }
+  # reorder by id
+  tbl <- tbl %>%
+    dplyr::select(1, rownames(m_data)) %>%
+    data.frame(row.names = 1)
 
-  names_match <- identical(m_data[[1]], names(tbl_group)[-1])
-  if (!names_match){cli::cli_abort(c(
-    "x"="Sample names in assay and metadata do not match."))}
-
-
-  # store data into matrix
-  m_groups <- as.matrix(unname(norm_group[, -1]))
-  m_strat <- as.matrix(unname(norm_strat[,-1]))
-
-  # create SE objects
-  se_groups <- SummarizedExperiment::SummarizedExperiment(assays=m_groups, colData=m_data)
-  se_strat <- SummarizedExperiment::SummarizedExperiment(assays=m_strat, colData=m_data)
-
-  return(list(se_groups=se_groups, se_strat=se_strat))
+  # create SE object
+  SummarizedExperiment::SummarizedExperiment(
+    assays = tbl,
+    colData = m_data
+  )
 }
 
 
+.humann_path_checks <- function(file_path, metadata) {
+  # check input
+  if (missing(file_path)) { cli::cli_abort(c("x" = "File path is missing")) }
+  if (missing(metadata)) { cli::cli_abort(c("x" = "Metadata is missing")) }
 
+  # check file path exists
+  if (!file.exists(file_path)) { cli::cli_abort(c("x" = "File does not exist")) }
+
+  # check metadata input
+  if (tools::file_ext(metadata) != "csv"){
+    cli::cli_abort(c(
+      "x" = "Unsupported metadata file extension.",
+      "i" = "Please provide a CSV file."
+    ))
+  }
+}
+
+.humann_tbl_checks <- function(humann_tbl) {
+  # check header
+  header <- colnames(humann_tbl)[1]
+  if (!(
+    stringr::str_detect(header, "^# Gene Family$") ||
+    stringr::str_detect(header, "^# Pathway$")
+  )) {
+    cli::cli_abort(c("x" = "The header of the file should be '# Gene Family' or '# Pathway'."))
+  }
+
+  # check number of columns
+  if (ncol(humann_tbl) < 2) {
+    cli::cli_abort(c(
+      "x" = "ncol(input) < 2.",
+      "i" = "The file should contain at least two columns."
+    ))
+  }
+
+  # check number of rows
+  if (nrow(humann_tbl) <= 1) {
+    cli::cli_abort(c(
+      "x"="The file does not contain any data",
+      "i"="Ensure the file is not empty."
+    ))
+  }
+
+  # check column classes
+  class_1 <- class(humann_tbl[[1]]) == "character"
+  class_2 <- purrr::map_lgl(tbl[-1], ~ class(.x) == "numeric") %>% unname()
+
+  if (!class_1 || !all(class_2)) {
+    cli::cli_abort(c(
+      "x" = "Column classes are not correct.",
+      "i" = "First column should be 'character' and the rest should be 'numeric'."
+    ))
+  }
+}
+
+.extract_norm_method <- function(tbl) {
+  # check data normalization
+  sum_counts <-
+    dplyr::filter(tbl, !stringr::str_detect(.data[[header]],"[|]")) %>%
+    dplyr::select(-1) %>%
+    colSums()
+
+  classification <-
+    ifelse(dplyr::between(sum_counts, 99900, 1001000), "cpm", "rpk") %>%
+    unique()
+
+  # Check if all are same normalization
+  if (length(classification) != 1) {
+    cli::cli_abort(c(
+      "x" = "Columns have mixed units.",
+      "i" = "Please ensure all samples have either 'rpk' or 'cpm' units."
+    ))
+  }
+
+  classification
+}
+
+.rpk2cpm <- function(tbl) {
+  # scale by cat
+  tbl_group <-
+    dplyr::filter(tbl, !stringr::str_detect(.data[[header]],"[|]")) %>%
+    dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ . / sum(.) * 1e3))
+
+  # scale by strat
+  tbl_strat <-
+    dplyr::filter(tbl, stringr::str_detect(.data[[header]], "[|]|UNMAPPED")) %>%
+    dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ . / sum(.) * 1e3))
+
+  dplyr::bind_rows(tbl_group, tbl_strat) %>%
+    dplyr::arrange(stringr::str_remove_all(`# Gene Family`, "[|].*"))
+}
+
+.humann_metadata_check <- function(meta_df, humann_tbl) {
+  # check metadata content
+  if (ncol(meta_df) < 2) {
+    cli::cli_abort(c(
+      "x" = "ncol(metadata) < 2.",
+      "i" = "Metadata should contain at least one annotation."
+    ))
+  }
+
+  # check sample ids
+  if (!all(meta_df[[1]] %in% names(humann_tbl))) {
+    cli::cli_abort(c("x" = "Sample names in assay and metadata do not match."))
+  }
+
+
+  meta_df
+}
